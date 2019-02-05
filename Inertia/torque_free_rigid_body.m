c472bfe46547e969f52b1d92ff05df13535a4cf7
Dmitry Savransky <dsavransky@gmail.com>
2019-02-04 22:19:14 -0500

    tfrb update

--- Inertia/torque_free_rigid_body.m
+++ Inertia/torque_free_rigid_body.m
@@ -8,79 +8,83 @@ addOptional(p,'t', linspace(0,100,1000),@(x) (numel(x) >=2) && ...
 addOptional(p,'angs0', [50,40,30]*pi/180, ...
     @(x) (numel(x) == 3) && isnumeric(x));  
 addOptional(p,'poinsot',false, @(x) isscalar(x) && islogical(x));
+addOptional(p,'xtrace',true, @(x) isscalar(x) && islogical(x));
+addOptional(p,'ytrace',false, @(x) isscalar(x) && islogical(x));
+addOptional(p,'ztrace',true, @(x) isscalar(x) && islogical(x));
 
-parse(p,width,varargin{:});
+parse(p,varargin{:});
 
 w0 = p.Results.w0(:);
 I = p.Results.I;
 t = p.Results.t;
+h = sqrt(I(1)^2*w0(1)^2 + I(2)^2*w0(2)^2 + I(3)^2*w0(3)^2); %ang momentum
 if p.Results.poinsot
-    h = sqrt(I(1)^2*w0(1)^2 + I(2)^2*w0(2)^2 + I(3)^2*w0(3)^2);
-    a = I(1)*w0(1) + I(2)*w0(2);
+    %orient initially such that h = -e_3
     psi0 = 0;
     theta0 = atan2(-sqrt(I(1)^2*w0(1)^2 + I(2)^2*w0(2)^2),-I(3)*w0(3));
     phi0 = atan2(I(1)*w0(1),I(2)*w0(2));
+    xtrace = false;
+    ytrace = false;
+    ztrace = false;
 else
+    %orient initially based on inputs/defaults
     psi0 = p.Results.angs0(1);
     theta0 = p.Results.angs0(2);
     phi0 = p.Results.angs0(3);
+    xtrace = p.Results.xtrace;
+    ytrace = p.Results.ytrace;
+    ztrace = p.Results.ztrace;
 end
 
-bCi313 = @(psi,theta,phi) [-sin(phi).*sin(psi).*cos(theta) + cos(phi).*cos(psi) sin(phi).*cos(psi).*cos(theta) + sin(psi).*cos(phi) sin(phi).*sin(theta); -sin(phi).*cos(psi) - sin(psi).*cos(phi).*cos(theta) -sin(phi).*sin(psi) + cos(phi).*cos(psi).*cos(theta) sin(theta).*cos(phi); sin(psi).*sin(theta) -sin(theta).*cos(psi) cos(theta)];
+omega2dang = @(psi,theta,phi,omega_1,omega_2,omega_3)...
+  [(omega_1.*sin(phi) + omega_2.*cos(phi))./sin(theta);...
+   omega_1.*cos(phi) - omega_2.*sin(phi);...
+ -omega_1.*sin(phi)./tan(theta) - omega_2.*cos(phi)./tan(theta) + omega_3];
 
-omega2dang = @(psi,theta,phi,omega_1,omega_2,omega_3) [(omega_1.*sin(phi) + omega_2.*cos(phi))./sin(theta);omega_1.*cos(phi) - omega_2.*sin(phi);-omega_1.*sin(phi)./tan(theta) - omega_2.*cos(phi)./tan(theta) + omega_3];
+dang2omega = @(psi,theta,phi,psidot,thetadot,phidot)...
+    [psidot.*sin(phi).*sin(theta) + thetadot.*cos(phi), ...
+    psidot.*sin(theta).*cos(phi) - thetadot.*sin(phi), ...
+    phidot + psidot.*cos(theta)];
 
-dang2omega = @(psi,theta,phi,psidot,thetadot,phidot) [psidot.*sin(phi).*sin(theta) + thetadot.*cos(phi), psidot.*sin(theta).*cos(phi) - thetadot.*sin(phi), phidot + psidot.*cos(theta)];
-
-
-
+%calculate initial angle derivatives
 dangs0 = omega2dang(psi0,theta0,phi0,w0(1),w0(2),w0(3));
 
+%create initial state
 z0 = [psi0,theta0,phi0,dangs0.'];
 
-[~,z2] = torque_free_motion3(t,z0,I);
+%integrate!
+[~,z] = torque_free_motion3(t,z0,I);
 
-ws2 = dang2omega(z2(:,1),z2(:,2),z2(:,3),z2(:,4),z2(:,5),z2(:,6));
-ws_I = zeros(size(ws2));
-hs_I = zeros(size(ws2));
-ws_norm = zeros(size(ws2));
-bCis = zeros(length(z2),3,3);
-polhode = zeros(size(ws2));
-for j = 1:length(z2)
-    tmp = EulerAngs2DCM(z2(j,1:3),[3,1,3]);
-    ws_norm(j,:) = ws2(j,:)/norm(ws2(j,:));
+%calculate time histories
+%body frame omegas:
+ws = dang2omega(z(:,1),z(:,2),z(:,3),z(:,4),z(:,5),z(:,6));
+ws_I = zeros(size(ws)); %inertial frame omegas
+hs_norm = zeros(size(ws)); %body frame ang momentum direction
+ws_norm = zeros(size(ws)); %body frame omega direction
+bCis = zeros(length(z),3,3); %DCMs
+polhode = zeros(size(ws)); %intersection of omega with ellipsoid in B frame
+for j = 1:length(z)
+    tmp = EulerAngs2DCM(z(j,1:3),[3,1,3]);
+    ws_norm(j,:) = ws(j,:)/norm(ws(j,:));
     ws_I(j,:) = (tmp.'*ws_norm(j,:).').';
-    hs_I(j,:) = (tmp.'*diag(I)*ws2(j,:).').';
+    hs_norm(j,:) = (diag(I)*ws(j,:).').'/h;
     bCis(j,:,:) = tmp;
-    polhode(j,:) = ws2(j,:)/sqrt((ws2(j,:)*diag(I)*ws2(j,:).'));
+    polhode(j,:) = ws(j,:)/sqrt((ws(j,:)*diag(I)*ws(j,:).'));
 end
 
-mats = bCis;
+mats = bCis; %hgtransform matrix (inertial frame)
 mats(:,4,4) = 1;
-bCa0 = squeeze(bCis(1,:,:));
- %% just a sanity check - not needed
-% bCa0 = squeeze(bCis(1,:,:));
-% z0 = [w0(:);bCa0(:)];
-% [~,z] = torque_free_motion2(t,z0,I);
-% 
-% bCas = reshape(z(:,4:end),length(z),3,3);
-% 
-% dets = zeros(length(bCas),1);
-% angs = zeros(length(bCas),3);
-% for j = 1:length(bCas)
-%     dets(j) = det(squeeze(bCas(j,:,:)));
-%     angs(j,:) = DCM2EulerAngs(squeeze(bCas(j,:,:)),[3,1,3]);
-% end
-% 
-% % mats = bCas;
-% % mats(:,4,4) = 1;
+bCi0 = squeeze(bCis(1,:,:)); %initial DCM
+h_I = bCi0.'*diag(I)*ws(1,:).'/h; %inertia angular momentum 
 
-%%
+%inertial frame
 figure(1)
 clf
+set(1,'Position',[61 251 560 420])
 g1 = hgtransform;
 axscale = 1.75;
 xscale = sqrt(1/I(1))*axscale;
+yscale = sqrt(1/I(2))*axscale;
 zscale = sqrt(1/I(3))*axscale;
 oscale  = 1.1;
 [xe,ye,ze]=ellipsoid(0,0,0,sqrt(1/I(1)),sqrt(1/I(2)),...
@@ -97,11 +101,19 @@ zax = quiver3(0,0,0,0,0,sqrt(1/I(3))*axscale,'Linewidth',3,...
 omegaax = quiver3(0,0,0,ws_norm(1,1)*oscale,ws_norm(1,2)*oscale,...
     ws_norm(1,3)*oscale,'Linewidth',3,'Color','k','Parent',g1,...
     'AutoScale','off','MaxHeadSize',0.5);
-hax = quiver3(0,0,0,hs_I(1,1),hs_I(1,2),hs_I(1,3),'Linewidth',3,...
-    'Color',[0.5,0.5,0.5],'AutoScale','off','MaxHeadSize',0.5); %inertially fixed
-xaxhist = plot3(bCa0(1,1)*xscale,bCa0(1,2)*xscale,bCa0(1,3)*xscale,'r--');
-zaxhist = plot3(bCa0(3,1)*zscale,bCa0(3,2)*zscale,bCa0(3,3)*zscale,'b--');
-omegaaxhist = plot3(ws_I(1,1)*oscale,ws_I(1,2)*oscale,ws_I(1,3)*oscale,'k--');
+hax = quiver3(0,0,0,h_I(1),h_I(2),h_I(3),'Linewidth',3,...
+    'Color',[0.5,0.5,0.5],'AutoScale','off','MaxHeadSize',0.5);
+if xtrace
+    xaxhist = plot3(bCi0(1,1)*xscale,bCi0(1,2)*xscale,bCi0(1,3)*xscale,'r--');
+end
+if ytrace
+    yaxhist = plot3(bCi0(2,1)*yscale,bCi0(2,2)*yscale,bCi0(2,3)*yscale,'g--');
+end
+if ztrace
+    zaxhist = plot3(bCi0(3,1)*zscale,bCi0(3,2)*zscale,bCi0(3,3)*zscale,'b--');
+end
+omegaaxhist = plot3(ws_I(1,1)*oscale,ws_I(1,2)*oscale,...
+    ws_I(1,3)*oscale,'k--');
 
 %set up lighting
 l = light('Position',[0 -1.5 0]);
@@ -112,17 +124,31 @@ view(3)
 axis equal
 axmx = max(max(1./sqrt(I))*axscale,1);
 axis([-axmx,axmx,-axmx,axmx,-axmx,axmx])
-tmp = squeeze(mats(1,1:3,1:3)).'*polhode(1,:).';
-invplane = fill3([-1,-1,1,1,-1]*axmx,[-1,1,1,-1,-1]*axmx,...
-    [1,1,1,1,1]*tmp(3),'c','FaceAlpha',0.2,'EdgeColor','None');
 
+if p.Results.poinsot
+    tmp = squeeze(mats(1,1:3,1:3)).'*polhode(1,:).';
+    invplanez = tmp(3);
+    invplane = fill3([-1,-1,1,1,-1]*axmx,[-1,1,1,-1,-1]*axmx,...
+        [1,1,1,1,1]*invplanez,'c','FaceAlpha',0.2,'EdgeColor','None');
+    
+    herpolhode = [invplanez./ws_I(:,3).*ws_I(:,1),...
+                  invplanez./ws_I(:,3).*ws_I(:,2),...
+                  zeros(length(t),1)+invplanez];
+    set(omegaaxhist,'ZData',invplanez,'Linestyle','-','Linewidth',2,...
+        'Color',[0.3,0.3,0.3],'XData',herpolhode(1,1),'YData',herpolhode(1,2))
+    
+    polhodehisti = plot3(polhode(1,1),polhode(1,2),polhode(1,3),'k',...
+        'LineWidth',2,'Parent',g1);
+end
 
 set(g1,'Matrix',squeeze(mats(1,:,:)).');
 set(gca,'Visible','off')
 camzoom(gca,2)
 
+%body frame
 figure(2)
 clf
+set(2,'Position',[622 249 560 420])
 s1 = surface(xe,ye,ze,'FaceAlpha',0.8,'SpecularExponent',10,...
     'SpecularStrength',0.3,'FaceColor','b');
 hold on
@@ -136,6 +162,9 @@ omegaaxb = quiver3(0,0,0,ws_norm(1,1)*axscale,ws_norm(1,2)*axscale,...
     ws_norm(1,3)*axscale,'Linewidth',3,'Color','k',...
     'AutoScale','off','MaxHeadSize',0.5);
 polhodehist = plot3(polhode(1,1),polhode(1,2),polhode(1,3),'k','LineWidth',2);
+haxb = quiver3(0,0,0,hs_norm(1,1)*axscale,hs_norm(1,2)*axscale,...
+    hs_norm(1,3)*axscale,'Linewidth',3,'Color',[0.5,0.5,0.5],...
+    'AutoScale','off','MaxHeadSize',0.5);
 
 lb = light('Position',[1.5 1.5 1.5]);
 lighting phong
@@ -148,25 +177,46 @@ axis([-axmx,axmx,-axmx,axmx,-axmx,axmx])
 set(g1,'Matrix',squeeze(mats(1,:,:)).');
 set(gca,'Visible','off')
 camzoom(gca,2.5)
-%%
-for j = 2:length(to)
+
+for j = 2:length(t)
     set(g1,'Matrix',squeeze(mats(j,:,:)).');
-    set(xaxhist,'XData',[get(xaxhist,'XData'),mats(j,1,1)*xscale],...
+    if xtrace
+        set(xaxhist,'XData',[get(xaxhist,'XData'),mats(j,1,1)*xscale],...
                 'YData',[get(xaxhist,'YData'),mats(j,1,2)*xscale],...
                 'ZData',[get(xaxhist,'ZData'),mats(j,1,3)*xscale])
-    set(zaxhist,'XData',[get(zaxhist,'XData'),mats(j,3,1)*zscale],...
+    end
+    if ytrace
+        set(yaxhist,'XData',[get(yaxhist,'XData'),mats(j,2,1)*yscale],...
+                'YData',[get(yaxhist,'YData'),mats(j,2,2)*yscale],...
+                'ZData',[get(yaxhist,'ZData'),mats(j,2,3)*yscale])    
+    end
+    if ztrace
+        set(zaxhist,'XData',[get(zaxhist,'XData'),mats(j,3,1)*zscale],...
                 'YData',[get(zaxhist,'YData'),mats(j,3,2)*zscale],...
                 'ZData',[get(zaxhist,'ZData'),mats(j,3,3)*zscale])
-    set(omegaaxhist,'XData',ws_I(1:j,1)*oscale,...
+    end
+    if p.Results.poinsot
+        set(omegaaxhist,'XData',herpolhode(1:j,1),...
+                    'YData',herpolhode(1:j,2),...
+                    'ZData',herpolhode(1:j,3))
+        set(polhodehisti,'XData',polhode(1:j,1),...
+                         'YData',polhode(1:j,2),...
+                         'ZData',polhode(1:j,3))
+    else
+        set(omegaaxhist,'XData',ws_I(1:j,1)*oscale,...
                     'YData',ws_I(1:j,2)*oscale,...
                     'ZData',ws_I(1:j,3)*oscale)
+    end
     set(omegaax,'UData',ws_norm(j,1)*oscale,'VData',ws_norm(j,2)*oscale,...
                 'WData',ws_norm(j,3)*oscale)
     set(omegaaxb,'UData',ws_norm(j,1)*axscale,'VData',ws_norm(j,2)*axscale,...
                 'WData',ws_norm(j,3)*axscale)
+    set(haxb,'UData',hs_norm(j,1)*axscale,'VData',hs_norm(j,2)*axscale,...
+                'WData',hs_norm(j,3)*axscale)
     set(polhodehist,'XData',polhode(1:j,1),...
                     'YData',polhode(1:j,2),...
                     'ZData',polhode(1:j,3))
 
-    pause((to(j)-to(j-1)));
+    pause((t(j)-t(j-1)));
+end
 end
