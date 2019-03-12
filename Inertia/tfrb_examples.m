95f0c1788c3e28c624f51007de61ac663ea92fbd
Dmitry Savransky <dsavransky@gmail.com>
2019-03-12 16:32:25 -0400

    updating TFRB notebook and listing examples for lecture

---
+++ Inertia/tfrb_examples.m
@@ -0,0 +1,12 @@
+binet_construction
+%%
+torque_free_rigid_body('w0', [1,0.1,0.1])
+
+%%
+torque_free_rigid_body('w0', [0.1,0.1,1])
+
+%%
+torque_free_rigid_body('w0', [0,2,0]+1e-3)
+
+%%
+torque_free_rigid_body('poinsot', true)
